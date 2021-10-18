a961c2108e8b1f98a0dd937bc73b7b1dfde06f75
Ringo <i17>
2021-10-18 17:48:26 +0200

    refactored to minimal version
    (cherry picked from commit 8a80076a3ac75fe6e5a1f535785b984c171deed0)

--- TextViewCrashDemo/ViewController.swift
+++ TextViewCrashDemo/ViewController.swift
@@ -7,50 +7,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  let tv1 = UITextView(frame: CGRect(x: 10, y: 110, width: 200, height: 40))
+  let tv2 = UITextView(frame: CGRect(x: 10, y: 210, width: 200, height: 40))
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    let tv = PlaceholderTextView(frame: CGRect(x: 10, y: 110, width: 200, height: 40))
-    let tv2 = PlaceholderTextView(frame: CGRect(x: 10, y: 210, width: 200, height: 40))
-    self.view.addSubview(tv)
+   
+    self.view.addSubview(tv1)
     self.view.addSubview(tv2)
-    tv.backgroundColor = .white
-    tv.placeholder = "Hello World!"
+    
+    tv1.backgroundColor = .white
+    tv1.text = "Hello World!"//placeholder
     
     tv2.backgroundColor = .white
-    tv2.placeholder = "Hello World!"
+    tv2.text = "Hello World!"
+    
+    tv1.delegate = self
+    tv2.delegate = self
+    
     self.view.frame = UIScreen.main.bounds
   }
 }
 
-class PlaceholderTextView: UITextView, UITextViewDelegate {
-  
-  var placeholder : String? {
-    didSet { verifyPlaceholder() }
-  }
-  
-  func verifyPlaceholder(){
-    if (self.text == "")
-    {
-      self.text = placeholder
-      self.textColor = .lightGray
-    }
-    else {
-      self.textColor = .blue
-    }
-  }
-  
-  override func willMove(toSuperview newSuperview: UIView?) {
-    super.willMove(toSuperview: newSuperview)
-    self.delegate = self
-  }
+extension ViewController: UITextViewDelegate {
   
   public func textViewDidBeginEditing(_ textView: UITextView)
   {
-    if (self.text == placeholder)
-    {
+    if textView.text == "Hello World!" {//Remove the placeholder text on begin
 //      textView.text = "" //Crash!
-      textView.textColor = .blue
     }
   }
 }
