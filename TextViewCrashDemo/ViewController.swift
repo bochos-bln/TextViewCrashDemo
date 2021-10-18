f3b9b090c01870d5d01aeab97e07c16e27b2ad54
Ringo <i17>
2021-10-18 17:38:48 +0200

    added demo code for crash
    (cherry picked from commit 8128d2f584b47c2f8afb541ed94c1056ca9a188f)

--- TextViewCrashDemo/ViewController.swift
+++ TextViewCrashDemo/ViewController.swift
@@ -4,16 +4,53 @@
 //
 //  Created by Ringo Müller on 18.10.21.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    let tv = PlaceholderTextView(frame: CGRect(x: 10, y: 110, width: 200, height: 40))
+    let tv2 = PlaceholderTextView(frame: CGRect(x: 10, y: 210, width: 200, height: 40))
+    self.view.addSubview(tv)
+    self.view.addSubview(tv2)
+    tv.backgroundColor = .white
+    tv.placeholder = "Hello World!"
+    
+    tv2.backgroundColor = .white
+    tv2.placeholder = "Hello World!"
+    self.view.frame = UIScreen.main.bounds
   }
-
-
 }
 
+class PlaceholderTextView: UITextView, UITextViewDelegate {
+  
+  var placeholder : String? {
+    didSet { verifyPlaceholder() }
+  }
+  
+  func verifyPlaceholder(){
+    if (self.text == "")
+    {
+      self.text = placeholder
+      self.textColor = .lightGray
+    }
+    else {
+      self.textColor = .blue
+    }
+  }
+  
+  override func willMove(toSuperview newSuperview: UIView?) {
+    super.willMove(toSuperview: newSuperview)
+    self.delegate = self
+  }
+  
+  public func textViewDidBeginEditing(_ textView: UITextView)
+  {
+    if (self.text == placeholder)
+    {
+//      textView.text = "" //Crash!
+      textView.textColor = .blue
+    }
+  }
+}
