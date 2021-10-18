c23906ed25072d883ca6e4f67cc7609facc70543
Ringo <i17>
2021-10-18 16:23:50 +0200

    Initial Commit
    (cherry picked from commit 0e93a668d127471688fa7b2a02c517f0dadefd66)

---
+++ TextViewCrashDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextViewCrashDemo
+//
+//  Created by Ringo Müller on 18.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
